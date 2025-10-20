fced462e3559a725de0fe4b7333a98433e93a57d
Rubab <rubab0592@gmail.com>
2025-10-21 03:53:20 +0500

    Order details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 import { CategoryPopupComponent } from './components/categories-popup/categories-popup';
 import { AddToCartComponent } from './components/add-to-cart/add-to-cart';
 import { OrderDetailsEmptyComponent } from './components/order-details-empty/order-details-empty';
-
+import { OrderDetailsItemsComponent } from './components/order-details/order-details';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, CategoryPopupComponent, AddToCartComponent,OrderDetailsEmptyComponent], // ✅ okay now
+  imports: [CommonModule, CategoryPopupComponent, AddToCartComponent,OrderDetailsEmptyComponent,OrderDetailsItemsComponent], // ✅ okay now
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
---
+++ src/app/components/order-details/order-details.ts
@@ -0,0 +1,68 @@
+import { Component } from '@angular/core';
+import { CommonModule, CurrencyPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-order-details',
+  standalone: true,
+  imports: [CommonModule, CurrencyPipe],
+  templateUrl: './order-details.html',
+  styleUrls: ['./order-details.scss']
+})
+export class OrderDetailsItemsComponent {
+  cartItems = [
+    {
+      name: 'Classic chicken burger',
+      color: 'Brown',
+      size: 'S',
+      quantity: 4,
+      price: 5.49,
+      image: 'assets/images/categories/burger.png'
+    },
+    {
+      name: 'Classic chicken burger',
+      color: 'Brown',
+      size: 'S',
+      quantity: 4,
+      price: 5.49,
+      image: 'assets/images/categories/burger.png'
+    },
+    {
+      name: 'Classic chicken burger',
+      color: 'Brown',
+      size: 'S',
+      quantity: 4,
+      price: 5.49,
+      image: 'assets/images/categories/burger.png'
+    },
+    {
+      name: 'Classic chicken burger',
+      color: 'Brown',
+      size: 'S',
+      quantity: 4,
+      price: 5.49,
+      image: 'assets/images/categories/burger.png'
+    }
+  ];
+
+  get subtotal() {
+    return this.cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0);
+  }
+
+  tax = 20.0;
+
+  get total() {
+    return this.subtotal + this.tax;
+  }
+
+  increaseQty(item: any) {
+    item.quantity++;
+  }
+
+  decreaseQty(item: any) {
+    if (item.quantity > 1) item.quantity--;
+  }
+
+  removeItem(item: any) {
+    this.cartItems = this.cartItems.filter(i => i !== item);
+  }
+}
