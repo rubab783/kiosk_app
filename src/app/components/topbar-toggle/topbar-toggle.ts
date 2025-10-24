f1eba790288bc2dfab08c86ec95d3403f65f67bf
Rubab <rubab0592@gmail.com>
2025-10-24 17:08:59 +0500

    Updated Css

--- src/app/components/topbar-toggle/topbar-toggle.ts
+++ src/app/components/topbar-toggle/topbar-toggle.ts
@@ -18,7 +18,19 @@ export class TopbarToggleComponent {
     { name: 'Pizza', icon: 'assets/images/categories/pizza.png' },
     { name: 'Wraps', icon: 'assets/images/categories/wraps.png' },
     { name: 'Steak', icon: 'assets/images/categories/steaks.png' },
-    { name: 'Kids Meal', icon: 'assets/images/categories/kidsmeal.png' },
+    { name: 'Meal', icon: 'assets/images/categories/chinese.png' },
+    { name: 'Burgers', icon: 'assets/images/categories/burger.png' },
+    { name: 'Chinese', icon: 'assets/images/categories/chinese.png' },
+    { name: 'Pizza', icon: 'assets/images/categories/pizza.png' },
+    { name: 'Wraps', icon: 'assets/images/categories/wraps.png' },
+    { name: 'Steak', icon: 'assets/images/categories/steaks.png' },
+    { name: 'Meal', icon: 'assets/images/categories/chinese.png' },
+    { name: 'Burgers', icon: 'assets/images/categories/burger.png' },
+    { name: 'Chinese', icon: 'assets/images/categories/chinese.png' },
+    { name: 'Pizza', icon: 'assets/images/categories/pizza.png' },
+    { name: 'Wraps', icon: 'assets/images/categories/wraps.png' },
+    { name: 'Steak', icon: 'assets/images/categories/steaks.png' },
+    { name: 'Meal', icon: 'assets/images/categories/chinese.png' },
   ];
 
   foods = [
